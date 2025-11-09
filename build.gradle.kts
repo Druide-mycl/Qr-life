bef31d0d4516b103d0c1c30ac341ab1253955ed2
Druide-mycl <bendickmatthias@gmail.com>
2025-11-09 13:30:02 +0100

    Refactor build.gradle.kts to use buildscript
    Updated build.gradle.kts to use buildscript block for dependencies.

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,4 +1,13 @@
-plugins {
-    id("com.android.application") version "8.5.2" apply false
-    kotlin("android") version "2.0.0" apply false
+// Root build.gradle.kts – legacy-kompatibel, funktioniert ohne pluginManagement
+buildscript {
+    repositories {
+        google()
+        mavenCentral()
+    }
+    dependencies {
+        classpath("com.android.tools.build:gradle:8.5.2")
+        classpath("org.jetbrains.kotlin:kotlin-gradle-plugin:2.0.0")
+    }
 }
+
+// keine Plugins hier anwenden – nur Klassenpfade bereitstellen
